5f9381b4da24007d1974f7add07e403798f4927c
dagorne <thomas.dagorne@etu.u-pec.fr>
2022-05-16 17:27:15 +0200

    Ajout pages

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="fr">
+<head>
+    <meta charset='utf-8'>
+    <title>IT+  Accueil</title>
+</head>
+<body>
+	<section class:"site">
+		<header>
+			<nav>
+                <label for="btn" class="icon-burger">
+                    <img class="burger" src="./images/burger.png" alt="burger">
+                </label>
+                <input class="input-burger" type="checkbox" id="btn">
+                <ul class="header_barre_nav">
+                    <li class="items"><a href="./pages/achat.php" class="achat">Achat</a></li>
+                    <li class="items"><a href="./pages/vente.php" class="vente">Vente</a></li>
+                    <li class="items"><a href="./pages/profil.php" class="profil">Mon profil</a></li>
+                </ul>
+            </nav>
+		</header>
+	</section>
+</body>
+</html>
---
+++ pages/vente.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+<head>
+    <meta charset='utf-8'>
+    <title>IT+ - Vente</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body>
+
+</body>
+</head>
+</html>
