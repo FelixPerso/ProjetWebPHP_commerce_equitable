dad42be126da49376a02752dca2b1c8038860fca
dagorne <thomas.dagorne@etu.u-pec.fr>
2022-05-23 14:58:51 +0200

    MaJ Projet PHP

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html> 
 <html lang="fr">
 <head>
     <meta charset='utf-8'>
@@ -11,44 +12,28 @@
             <div class="rect-fond">
 
             </div>
-            <div class="bouton-burger">
-                <div class="barre"></div>
-                <div class="barre"></div>
-                <div class="barre"></div>
-            </div>
-                <div class="nav">
-                <ul class="header_barre_nav">
-                    <li class="items"><a href="./pages/achat.php" class="achat">Achat</a></li>
-                    <li class="items"><a href="./pages/vente.php" class="vente">Vente</a></li>
-                    <li class="items"><a href="./pages/profil.php" class="profil">Mon profil</a></li>
-                </ul>
+            <div class="menu-burger">
+                <div class="bouton-burger">
+                    <div class="barre"></div>
+                    <div class="barre"></div>
+                    <div class="barre"></div>
+                </div>
+                    <div class="nav">
+                    <ul class="header_barre_nav">
+                        <li class="items"><a href="./pages/achat.php" class="achat">Achat</a></li>
+                        <li class="items"><a href="./pages/vente.php" class="vente">Vente</a></li>
+                        <li class="items"><a href="./pages/profil.php" class="profil">Mon profil</a></li>
+                    </ul>
+                </div>
             </div>
         </header>
         <div class="grand-rectangle1">
             <div class="rectangle1">
-                <h3>TIRAGES AU SORT</h3>
+                <h3>Achat</h3>
                 <p class="phrases">Participez et tentez d'être l'un des heureux élus devenant propriétaire d'une paire de quantité limitée.</p>
-                <a href="./pages/services/tirages/tirages.html">
+                <a href="./pages/achat.php">
                     <div class="bouton">
-                        <p>DÉCOUVRIR !</p>
-                    </div>
-                </a>
-            </div>
-            <div class="rectangle2">
-                <h3>VENTE & ACHAT</h3>
-                <p class="phrases">Vendez et achetez vos paires entre particuliers.</p>
-                <a href="./pages/services/vente-achat/vente-achat.html">
-                    <div class="bouton">
-                        <p>DÉCOUVRIR !</p>
-                    </div>
-                </a>
-            </div>
-            <div class="rectangle3">
-                <h3>PERSONNALISATION</h3>
-                <p class="phrases">Personnalisez l'une de vos paires avec nos options proposées.</p>
-                <a href="./pages/services/personnalisation.html">
-                    <div class="bouton">
-                        <p>DÉCOUVRIR !</p>
+                        <p>VOIR</p>
                     </div>
                 </a>
             </div>
