3e0072b0f80b7d169efd35057989be3eec07c43a
dagorne <thomas.dagorne@etu.u-pec.fr>
2022-05-24 16:50:29 +0200

    test

--- index.php
+++ index.php
@@ -9,27 +9,42 @@
 <body>
 	<section class="site">
         <header>
-            <div class="rect-fond">
-
+            <div class="bouton-burger">
+                <div class="barre"></div>
+                <div class="barre"></div>
+                <div class="barre"></div>
             </div>
-                <div class="bouton-burger">
-                    <div class="barre"></div>
-                    <div class="barre"></div>
-                    <div class="barre"></div>
-                </div>
-                <div class="nav">
+            <div class="nav">
                 <ul class="header_barre_nav">
                     <li class="items"><a href="./pages/achat.php" class="achat">Achat</a></li>
                     <li class="items"><a href="./pages/vente.php" class="vente">Vente</a></li>
                     <li class="items"><a href="./pages/profil.php" class="profil">Mon profil</a></li>
                 </ul>
-                </div>
+            </div>
         </header>
         <div class="grand-rectangle1">
             <div class="rectangle1">
                 <h3>Achat</h3>
                 <p class="phrases">Participez et tentez d'être l'un des heureux élus devenant propriétaire d'une paire de quantité limitée.</p>
                 <a href="./pages/achat.php">
+                    <div class="bouton">
+                        <p>VOIR</p>
+                    </div>
+                </a>
+            </div>
+            <div class="rectangle2">
+                <h3>Profil</h3>
+                <p class="phrases">Participez et tentez d'être l'un des heureux élus devenant propriétaire d'une paire de quantité limitée.</p>
+                <a href="./pages/profil.php">
+                    <div class="bouton">
+                        <p>VOIR</p>
+                    </div>
+                </a>
+            </div>
+            <div class="rectangle3">
+                <h3>Vente</h3>
+                <p class="phrases">Participez et tentez d'être l'un des heureux élus devenant propriétaire d'une paire de quantité limitée.</p>
+                <a href="./pages/vente.php">
                     <div class="bouton">
                         <p>VOIR</p>
                     </div>
