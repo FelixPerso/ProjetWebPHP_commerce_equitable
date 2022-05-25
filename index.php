eb9e8bc47ad0985715f6bc621000bca0571cc78f
dagorne <thomas.dagorne@etu.u-pec.fr>
2022-05-25 15:42:09 +0200

    MaJ ProjetPHP

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE HTML>
 <html lang="fr">
 <head>
     <meta charset='utf-8'>
@@ -25,7 +24,7 @@
         <div class="grand-rectangle1">
             <div class="rectangle1">
                 <h3>Achat</h3>
-                <p class="phrases">Participez et tentez d'être l'un des heureux élus devenant propriétaire d'une paire de quantité limitée.</p>
+                <p class="phrases">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
                 <a href="./pages/achat.php">
                     <div class="bouton">
                         <p>VOIR</p>
@@ -34,7 +33,7 @@
             </div>
             <div class="rectangle2">
                 <h3>Profil</h3>
-                <p class="phrases">Participez et tentez d'être l'un des heureux élus devenant propriétaire d'une paire de quantité limitée.</p>
+                <p class="phrases">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
                 <a href="./pages/profil.php">
                     <div class="bouton">
                         <p>VOIR</p>
@@ -43,7 +42,7 @@
             </div>
             <div class="rectangle3">
                 <h3>Vente</h3>
-                <p class="phrases">Participez et tentez d'être l'un des heureux élus devenant propriétaire d'une paire de quantité limitée.</p>
+                <p class="phrases">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
                 <a href="./pages/vente.php">
                     <div class="bouton">
                         <p>VOIR</p>
@@ -51,9 +50,9 @@
                 </a>
             </div>
         </div>
-        <h1>⎯⎯⎯⎯⎯⎯⎯ NOTRE CONCEPT ⎯⎯⎯⎯⎯⎯⎯</h1>
+        <h1>╾⎯⎯⎯⎯⎯⎯╼ NOTRE CONCEPT ╾⎯⎯⎯⎯⎯⎯╼</h1>
         <p class="p-concept">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus </p>
-        <h1 class="trait">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</h1>
+        <h1 class="trait">╾⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯╼</h1>
     </section>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
