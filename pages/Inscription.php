4f6bd25eb3b67da14ae09555b0be9ec55381d875
brenet <hugo.brenet@etu.u-pec.fr>
2022-06-03 15:59:45 +0200

    inscription

--- pages/Inscription.php
+++ pages/Inscription.php
@@ -3,43 +3,76 @@
 $showAlert = false; 
 $showError = false;
 $exists=false; 
+$error=0;
 include 'bd.php';     
 
     if (isset($_POST['Inscription'])) {
     // Include file which makes the
     // Database Connection.  
     
-    $login = $_POST["login"]; 
+    $login = $_POST["login"];
+    $firstName = $_POST["firstName"]; 
+    $name = $_POST["name"];  
     $mdp = $_POST["mdp"]; 
     $cmdp = $_POST["cmdp"];
             
     
-    $sql = "Select * from user where login='$login'";
+    $sql = "Select * from customer where login='$login'";
     
-    $result = mysqli_query($connexion, $sql);
+    $result = mysqli_query($conn, $sql);
     $num = mysqli_num_rows($result); 
     echo "$num";
+
      
-    strip_tags($mdp);
     // This sql query is use to check if
     // the login is already present 
     // or not in our Database
     if($num == 0) {
-        if(($mdp == $cmdp) && $exists==false) {
+            
+            if (empty($login) || strlen($login) > 50 || preg_match("^[A-Za-z '-]+$",$login)) {
+                echo "Votre login contient des caractères non acceptés";
+                $error++;
+            }
+
+            if (empty($name) || strlen($name) > 50 || preg_match("^[A-Za-z '-]+$",$name)) {
+                echo "Votre nom contient des caractères non acceptés";
+                $error++;
+            }
+
+            if (empty($firstName) || strlen($firstName) > 50 || preg_match("^[A-Za-z '-]+$",$firstName)) {
+                echo "Votre prénom contient des caractères non acceptés";
+                $error++;
+            }
+
+            if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "Votre email n'est pas correcte. Veuillez respecter un email du type : test@gmail.fr";
+                $error++;
+            }
+
+            if ($mdp != $cmdp) {
+                echo "Les mots de passes ne correspondent pas !";
+                $error++;
+            }
+
+            if ($error==0) {
     
             $hashed_password = password_hash($mdp,PASSWORD_DEFAULT);
                 
             // mdp Hashing is used here. 
-            $sql = "INSERT INTO user VALUES ('$login', 
-                '$hashed_password')";
-    
-            $result = mysqli_query($connexion, $sql);
+            $sql = "INSERT INTO customer(id, login, stash, mdp) VALUES (?,$login,0,'$hashed_password')";
+
+            $result = mysqli_query($conn, $sql); 
+
+            $sql = "INSERT INTO CustomerProtectedData(id, surname, firstName, email) VALUES (?,$surname,$firstName,$email)";
+
+            $result = mysqli_query($conn, $sql);
     
             if ($result) {
                 $showAlert = true; 
                 echo "ok";
             }
-        } 
+        }
+    } 
         else { 
             $showError = true; 
             echo "mdp pas correcte";
@@ -50,7 +83,6 @@ include 'bd.php';
    {
       echo "Le login est déjà utilisé !"; 
    } 
-}
   
     
 ?>
@@ -61,6 +93,22 @@ include 'bd.php';
             <label for="login">login:</label>
             <input type="login" id="login" name="login">
         </div>
+
+        <div>
+            <label for="name">Prenom:</label>
+            <input type="name" id="name" name="firstName">
+        </div>
+
+        <div>
+            <label for="name">Nom:</label>
+            <input type="name" id="name" name="name">
+        </div>
+
+        <div>
+            <label for="email">email:</label>
+            <input type="email" id="email" name="email">
+        </div>
+
         <div>
             <label for="mdp">mdp:</label>
             <input type="password" id="password" name="mdp">
@@ -70,6 +118,8 @@ include 'bd.php';
             <label for="mdp">cmdp:</label>
             <input type="password" id="password" name="cmdp">
         </div>
+
+
         <div class="button">
             <button type="submit" name="Inscription">S'inscrire</button>
         </div>
